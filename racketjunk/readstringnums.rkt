0feaa152c142b1bca59dc08028761d882c522dcd
tone.chromebook <tonetheman@gmail.com>
2019-11-28 23:27:17 -0600

    more comments

--- racketjunk/readstringnums.rkt
+++ racketjunk/readstringnums.rkt
@@ -3,7 +3,15 @@
 ;; example of possible input
 (define a "10 20\n30 40\n")
 
+;; this is the for loop
+;; in-list takes the result of string-split and makes a stream
 (for/list ([ln (in-list (string-split a "\n"))])
+
+	  ;; each ln is printed and it is the values
+	  ;; from each line in the file
 	  (printf "ln is ~a\n" ln)
+
+	  ;; then we call map and produce numbers
+	  ;; for the string values and split on space
 	  (map string->number (string-split ln " "))
   )

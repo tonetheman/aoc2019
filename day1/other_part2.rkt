350925a34c9023f555a0ea2037f41db3b83cdcf8
tonetheman <tonetheman@gmail.com>
2019-12-01 07:58:38 -0600

    more comments on other_part2

--- day1/other_part2.rkt
+++ day1/other_part2.rkt
@@ -3,21 +3,40 @@
 (println "part2")
 
 (define (calc n)
+  ;; definition from problem
   (- (quotient n 3) 2))
 
+;; a way to accumlate a list
+;; of results
 (define (listcalc n acc)
   (if (< n 0)
-      acc
+      ;; base case if accumulation is less than 0
+      ;; just return it you are done
+      acc 
+
+      ;; otherwise recurse
+      ;; pass the new number as n
+      ;; pass a list with the new number stuck on the front
+      ;; i calculated the number twice meh
       (listcalc (calc n) (cons (calc n) acc))
       )
   )
 
+;; since accumlator is ugly
+;; make a func that only takes
+;; the number
 (define (supercalc n)
+
+  ;; filter out the numbers that are less than 0
+  ;; add all the numbers all up
   (apply + (filter (lambda (n) (> n 0)) (listcalc n '())))
   )
 
+;; sum everything
 (apply +
+;; map supercalc across the list
 (map supercalc
+    ;; cast to numbers
     (map string->number 
         ;; read the file into a list
         (file->lines "data.txt")

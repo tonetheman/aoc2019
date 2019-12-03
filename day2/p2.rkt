493a835db886633a147f466b0fe2b20c5542fa10
tone.chromebook <tonetheman@gmail.com>
2019-12-02 20:59:18 -0600

    working p2.rkt

--- day2/p2.rkt
+++ day2/p2.rkt
@@ -1,13 +1,5 @@
 #lang racket
 
-(define s (file->string "data.txt"))
-;; (define s "1,9,10,3,2,3,11,0,99,30,40,50")
-;;(define s "1,1,1,4,99,5,6,0,99")
-
-(define inp
-    (list->vector (map string->number (string-split s ",")))
-)
-
 (define (opadd? n) (= n 1))
 (define (opadd-len) 4)
 
@@ -18,44 +10,73 @@
 (define (opterm-len) 1)
 
 (define (runprogram inp noun verb)
-    (println inp)
-
+ 
     (vector-set! inp 1 noun)
     (vector-set! inp 2 verb)
-    (println inp)
-    
     
     (let loop ([ip 0])
         (if (> (vector-length inp) ip)
             (cond
                 [(opadd? (vector-ref inp ip))
-                    (printf "opadd is called ~a\n" ip)
+                    ;; (printf "opadd is called ~a\n" ip)
                     (define arg1 (vector-ref inp (+ 1 ip)))
                     (define arg2 (vector-ref inp (+ 2 ip)))
                     (define output (vector-ref inp (+ 3 ip)))
-                    (printf "\t~a ~a ~a\n" arg1 arg2 output)
+                    ;; (printf "\t~a ~a ~a\n" arg1 arg2 output)
                     (vector-set! inp output (+ (vector-ref inp arg1) 
                         (vector-ref inp arg2)))
                     (loop (+ ip (opadd-len)))]
                 [(opmult? (vector-ref inp ip))
-                    (printf "opmult is called ~a\n" ip)
+                    ;; (printf "opmult is called ~a\n" ip)
                     (define arg1 (vector-ref inp (+ 1 ip)))
                     (define arg2 (vector-ref inp (+ 2 ip)))
                     (define output (vector-ref inp (+ 3 ip)))
-                    (printf "\t~a ~a ~a\n" arg1 arg2 output)
+                    ;; (printf "\t~a ~a ~a\n" arg1 arg2 output)
                     (vector-set! inp output (* (vector-ref inp arg1) 
                         (vector-ref inp arg2)))
                     (loop (+ ip (opmult-len)))]
                 [(opterm? (vector-ref inp ip))
-                    (printf "terminate!\n")
-                    (loop (+ ip (opterm-len)))]
+                    ;;(printf "terminate!\n")
+                    ;; this is a cheat to push the ip way out
+                    ;; that way the machine will stop
+                    (loop (+ ip (vector-length inp)))]
                 [else (println "nope")]
             )
             #f
         )
     )
-    (println inp)
-    (printf "the output is ~a\n" (vector-ref inp 0))
+    ;; return what is at pos 0
+    ;; as output
+    (vector-ref inp 0)
 )
 
-(runprogram (vector-copy inp) 12 2)
+(define (part1)
+    (define s (file->string "data.txt"))
+    ;; (define s "1,9,10,3,2,3,11,0,99,30,40,50")
+    ;;(define s "1,1,1,4,99,5,6,0,99")
+
+    (define inp
+        (list->vector (map string->number (string-split s ",")))
+    )
+    (runprogram (vector-copy inp) 12 2)
+)
+
+
+(define (part2)
+    (define s (file->string "data.txt"))
+
+    (define inp
+        (list->vector (map string->number (string-split s ",")))
+    )
+
+    (for ([noun 100])
+        (for ([verb 100])
+            (if (= (runprogram (vector-copy inp) noun verb) 19690720)
+                (println (+ (* 100 noun) verb))
+                '()
+            )
+        )
+    )
+)
+
+(part2)

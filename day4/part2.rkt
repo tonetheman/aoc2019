9764d2f1929fb8ea50277d74b53de592ae0489a6
tonetheman <tonetheman@gmail.com>
2019-12-04 06:39:52 -0600

    working on part2

--- day4/part2.rkt
+++ day4/part2.rkt
@@ -10,7 +10,7 @@
           (loop (quotient n b)
                 (cons (modulo n b) d))))))
 
-(define (has-eyes ll)
+(define (has-eyes-orig ll)
     (let ([d0 (list-ref ll 0)]
     [d1 (list-ref ll 1)]
     [d2 (list-ref ll 2)]
@@ -36,6 +36,84 @@
     
     )
 )
+
+;; not working :()
+(define (has-eyes2 ll)
+    (let ([d0 (list-ref ll 0)]
+    [d1 (list-ref ll 1)]
+    [d2 (list-ref ll 2)]
+    [d3 (list-ref ll 3)]
+    [d4 (list-ref ll 4)]
+    [d5 (list-ref ll 5)])
+        (if (= d0 d1) ;; 2 match in pos 0
+            (if (= d1 d2)
+                #f ;; part of a bigger group
+                (if (= d1 d2) ;; 2 match in pos 1
+                    (if (or (= d2 d3) (= d1 d0))
+                        #f ;; part of bigger group
+                        (if (= d2 d3) ;; 2 match in pos 2
+                            (if (or (= d3 d4) (= d2 d1))
+                                #f
+                                (if (= d3 d4) ;; 2 match in pos 2
+                                    (if (or (= d3 d2) (= d4 d5))
+                                        #f
+                                        (if (= d4 d5)
+                                            (if (= d4 d3)
+                                                #f
+                                                #t
+                                            )
+                                            #f  
+                                        )
+                                    )
+                                    #f
+                                )
+                            )
+                            #f
+                        )                       
+                    )
+                    #f
+                )
+            )
+            #f
+        )
+    )
+)
+
+(define (has-eyes3 ll)
+    (let ([d0 (list-ref ll 0)]
+    [d1 (list-ref ll 1)]
+    [d2 (list-ref ll 2)]
+    [d3 (list-ref ll 3)]
+    [d4 (list-ref ll 4)]
+    [d5 (list-ref ll 5)])
+    
+        ;; pos 0 = pos 1
+        (if (and (= d0 d1) (not (= d1 d2)))
+            (println "yup")
+            
+            ;; pos 1 = pos 2
+            (if (and (= d1 d2) (not (= d1 d0)) (not (= d2 d3)))
+                (println "yup")
+                
+                ;; pos 3 = pos 4
+                (if (and (= d3 d4) (not (= d3 d2)) (not (= d4 d5)))
+                    (println "yup")
+                    
+                    (if (and (= d4 d5) (not (= d4 d3))  )
+                        (println "yup")
+                        (println "nope")
+                    )
+
+                ) ;; end of pos 3 = pos 4
+            ) ;; end of pos 1 = pos 3
+
+        ) ;; end pos 0 = pos 1
+
+
+    );; end of let
+) ;; end of has-eyes3
+
+
 
 (define (always-incr ll)
     (let ([d0 (list-ref ll 0)]
@@ -64,14 +142,20 @@
 )
 
 
-(define count 0)
-(for ([i (in-range lowend hiend)])
-    (if (always-incr (digits i))
-        (if (has-eyes (digits i))
-            (set! count (+ count 1))
+(define (part2)
+    (define count 0)
+    (for ([i (in-range lowend hiend)])
+        (if (always-incr (digits i))
+            (if (has-eyes-orig (digits i))
+                
+                (set! count (+ count 1))
+                #f
+            )
             #f
         )
-        #f
     )
+    (println count)
 )
-(println count)
+
+(part2)
+;; (has-eyes3 (list 1 2 3 5 5 5))

bf73ea9cc005c75dce70a4e6ec9aef72e27ea127
tonetheman <tonetheman@gmail.com>
2019-12-08 16:40:41 -0600

    p2 stuff not working think it is from multiple intersections

--- day3/p2.rkt
+++ day3/p2.rkt
@@ -132,11 +132,10 @@ bob
     (if (equal? i '(0 0))
       #f
       (let ([d (dist '(0 0) i)])
-        (printf "~a ~a\n" i d)
+        (printf "~a ~a ~a ~a\n" i d (find-in-set i res1) (find-in-set i res2))
         (if (< d lowest)
           (begin
             (set! lowest d)
-            (printf "found it ~a\n" (find-in-set i res1))
             #t
           )
           #f
@@ -185,8 +184,8 @@ bob
 )
 
 ;; works
-(part1 "R8,U5,L5,D3" "U7,R6,D4,L4")
-;; (part1 "R75,D30,R83,U83,L12,D49,R71,U7,L72" "U62,R66,U55,R34,D71,R55,D58,R83")
+;; (part1 "R8,U5,L5,D3" "U7,R6,D4,L4")
+(part1 "R75,D30,R83,U83,L12,D49,R71,U7,L72" "U62,R66,U55,R34,D71,R55,D58,R83")
 ;; (part1 "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51" "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
 ;; (test-file)
 

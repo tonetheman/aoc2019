8cd2eee86f133eed1948edef368f37866ebbed1e
Tony Colston <tonetheman@gmail.com>
2019-12-10 12:50:50 -0600

    input seems to be working

--- day7/exper.rkt
+++ day7/exper.rkt
@@ -9,12 +9,17 @@
 (define (opterm? n) (= n 99))
 (define (opterm-len) 1)
 
-(struct machine (memory ip terminated) #:mutable )
+;; new opcodes  or day 5
+(define (opinput? n) (= n 3))
+(define opinput-len 2)
+
+(struct machine (memory ip terminated input) #:mutable )
 
 (define (repr m)
     (printf "ip is ~a\n" (machine-ip m))
     (printf "term is ~a\n" (machine-terminated m))
     (printf "memory is ~a\n" (machine-memory m))
+    (printf "input is ~a\n" (machine-input m))
 )
 
 (define (tranlate-opcode n)
@@ -24,6 +29,12 @@
     (define param2mode (string->number (substring s 1 2)))
     (define param3mode (string->number (substring s 0 1)))
     (list opcode param1mode param2mode param3mode)
+)
+
+;; not really the best way to do this
+;; a convenience func
+(define (add-input m n)
+    (set-machine-input! m (cons n (machine-input m)))
 )
 
 (define (handle-opadd m pm1 pm2 pm3)
@@ -84,19 +95,30 @@
 )
 
 (define (handle-opinput m pm1 pm2 pm3)
-    (define arg1 (vector-ref (machine-memory m) (+ 1 (machine-ip m))))
-    (printf "opinput called ~a\n" arg1)
-    ;; need to store the input variable
-    ;; passed into the function
-    ;; in this place in memory
-    ;; take the first one
-    (vector-set! (machine-memory m) arg1 (first input-to-program))
-    ;; pop the first input out of the way
-    ;; so that the next input will get the next
-    ;; value
-    (set! input-to-program (rest input-to-program))
-    ;; (printf "DBG: after opinput ~a\n" inp)         
-    (set-machine-ip! m (+ (machine-ip m) (opinput-len)))
+
+    ;; THIS BLOCKS THE MACHINE UNTIL WE GET INPUT
+    
+    (if (empty? machine-input)
+        #f ;; do nothing no input
+            ;; this is really blocking since
+            ;; we are not going to increment the ip
+        (let ([arg1 (vector-ref (machine-memory m) (+ 1 (machine-ip m)))])
+            ;; need to store the input variable
+            ;; passed into the function
+            ;; in this place in memory
+            ;; take the first one
+            (vector-set! (machine-memory m) arg1 (first (machine-input m)))
+            ;; pop the first input out of the way
+            ;; so that the next input will get the next
+            ;; value
+            (set-machine-input! m (rest (machine-input m)))
+            ;;(set! input-to-program (rest input-to-program))
+            ;; (printf "DBG: after opinput ~a\n" inp)   
+
+            (set-machine-ip! m (+ (machine-ip m) opinput-len))
+
+        ) ;; end of let
+    ) ;; end of if
 )
 
 
@@ -125,6 +147,7 @@
         ) ;; end  of match-let
     ) ;; end of _opcode let
 )
+
 (define (cycle m)
     (if (machine-terminated m)
         (println "warning: machine is terminated no action taken")
@@ -132,9 +155,16 @@
     )
 )
 
+(define (make-computer)
+    (machine (make-vector 1) ;; memory
+     0 ;; instruction pointer 
+     #f ;; terminated
+     '() ;; input
+     )
+)
 
 (define (test1)
-    (define computer (machine (make-vector 1) 0 #f))
+    (define computer (make-computer))
     (repr computer)
 
     (define instr (make-vector 100))
@@ -155,4 +185,24 @@
 
 )
 
-(test1)
+(define (test2)
+    (define computer (make-computer))
+    (define instr (make-vector 20))
+    (define counter 0)
+    (for ([i (list 3 15 3 16 99)])
+        (vector-set! instr counter i)
+        (set! counter (add1 counter))
+    )
+    (set-machine-memory! computer instr)
+    (add-input computer 42)
+    (add-input computer 43)
+    (printf "current input  ~a\n" (machine-input computer))
+    
+    (cycle computer)
+    (repr computer)
+    (cycle computer)
+    (repr computer)
+)
+
+;; (test1)
+(test2)

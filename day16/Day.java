16fa662cb2e7ff7ad69a0152eda6edca1e2a6795
tone.chromebook <tonetheman@gmail.com>
2019-12-16 20:57:09 -0600

    part1 working in java and much faster than racket

--- day16/Day.java
+++ day16/Day.java
@@ -33,6 +33,7 @@ public class Day {
         }
         return al;
     }
+
     public ArrayList<Integer> buildshiftpat(ArrayList<Integer> input) {
         ArrayList<Integer> al = new ArrayList<Integer>();
         for (int i=1;i<input.size();i++) {
@@ -42,20 +43,13 @@ public class Day {
     }
 
     public int crazy(int i, ArrayList<Integer> pat, ArrayList<Integer> shpat) {
-        int cl = pat.size();
-        System.out.println("\tcrazy: cl: " + cl + " i: "+i);
-        if (i<(cl-1)) {
-            if (i==0) {
-                return shpat.get(0);
-            }
-            return shpat.get(cl%i);
+        int cl = shpat.size();
+        if (i<cl) {
+            return shpat.get(i);
         } else {
-            int tmp = i-(cl-1);
-            if (tmp==0) {
-                return pat.get(0);
-            }
-            return pat.get(cl%tmp);
-        }    
+            i -= cl;
+            return pat.get(i%pat.size());
+        }
     }
     
     public ArrayList<Integer> parseInput(String inp) {
@@ -69,30 +63,51 @@ public class Day {
     }
 
     public int testcase(ArrayList<Integer> input, int cp) {
-        ArrayList<Integer> pat = buildpat(cp);
+        //System.out.println("testcase called cp: " + cp);
+        ArrayList<Integer> pat = buildpat(cp+1);
         ArrayList<Integer> shpat = buildshiftpat(pat);
+        //System.out.println("pat : " + pat);
+        //System.out.println("shpat : " + shpat);
         int res = 0;
         for(int i=0;i<input.size();i++) {
             int index = crazy(i,pat,shpat);
-            res += input.get(i) * index;
+            //System.out.println("\t i : " + i + " index: " + index);
+            int tmp = input.get(i) * index;
+            //System.out.println("\t" + input.get(i) + " " + tmp);
+            res += tmp;
         }
-
-        return res;
+        //System.out.println("res: " + res);
+        return Math.abs(res) % 10;
     }
 
     public ArrayList<Integer> runone(ArrayList<Integer> input) {
         ArrayList<Integer> al = new ArrayList<Integer>();
 
-        for(int i=1;i<input.size();i++) {
+        for(int i=0;i<input.size();i++) {
             al.add(testcase(input,i));
         }
         return al;
+    }
+
+    public void example2() {
+        String inp = "80871224585914546619083218645595";
+        ArrayList<Integer> al_input = parseInput(inp);
+        for(int i=0;i<100;i++) {
+            ArrayList<Integer> res = runone(al_input);
+            System.out.println(res);
+            for (int j=0;j<res.size();j++) {
+                al_input.set(j, res.get(j));
+            }
+        }
     }
 
     public void example1() {
         String inp = "12345678";
         ArrayList<Integer> al_input = parseInput(inp);
         System.out.println(al_input);
+        //ArrayList<Integer> res = runone(al_input);
+        //System.out.println(res);
+        
         for(int i=0;i<4;i++) {
             ArrayList<Integer> res = runone(al_input);
             System.out.println(res);
@@ -100,6 +115,7 @@ public class Day {
                 al_input.set(j, res.get(j));
             }
         }
+        
     }
 
     public void part1() {
@@ -118,8 +134,9 @@ public class Day {
 
     public static void main(String[] args) {
         Day app = new Day();
-        app.example1();
-        //app.part1();
-        
+        //app.example1();
+        app.part1();
+        //app.example2();
+
     }
 }
